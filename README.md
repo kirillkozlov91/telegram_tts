ab4c548e3310e174a6a61d5a6b305cc7fc4ceeab
Kirill Kozlov <kirillkozlov91@gmail.com>
2023-04-03 21:54:58 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# telegram_tts
-Telegram Text to Speech script that reads messages from group chats without using bots
+# Telegram Text To Speech Script for Group Chats
+
+Created with the help of ChatGPT 3.5
+
+This is a Python script to read Telegram messages locally and read them aloud through Text To Speech. 
+<br>
+Supports Cyrillic words. Made for macOS to run with maOS voice libraries. Tested on macOS only.
+
+Replace the APP IDs in the script with your IDs that you can find here:
+<br>
+https://my.telegram.org/apps
+
+Replace the phone number with your phone number that you use for Telegram, in the following format:
+<br>
++18005551234
+
+Run the script via the following command:
+<br>
+`python3 telebot.py`
+
+If it complains about any missing packages, install them via:
+<br>
+`pip3 install <missingpackagename>`
+
+Once you run the script, it will fetch the latest 20 most recent active chats.
+You will select some chat, and the script will start listening for new incoming messages for that chat.
+Using Apple's TTS it will read messages aloud and will print the messages as it goes through each one.
+
+URLs, stickers, attachements are ignored.
+If you know how to make them work, please reach out or feel free to fork
---
+++ telebot.py
@@ -0,0 +1,80 @@
+from telethon import TelegramClient, events
+import re
+import pyttsx3
+import AppKit
+import time
+from queue import Queue
+
+# Replace the values below with your own API ID, API hash, and phone number
+api_id = 'YOUR_API_ID'
+api_hash = 'YOUR_API_HASH'
+phone_number = '+1YOUR_PHONE_NUMBER'
+speaking_rate = 400
+
+# Connect to Telegram API
+client = TelegramClient('session_name', api_id, api_hash)
+
+# Start the client and authenticate with your phone number
+client.start(phone=phone_number)
+
+# Initialize the NSSpeechSynthesizer text-to-speech engine for the message text
+message_engine = AppKit.NSSpeechSynthesizer.alloc().initWithVoice_(AppKit.NSSpeechSynthesizer.defaultVoice())
+
+# Set the speaking rate of the speech synthesizer
+message_engine.setRate_(speaking_rate)
+
+# Define an asynchronous message handler function
+@client.on(events.NewMessage)
+async def handle_message(event):
+    # Ignore messages with no text or with URLs, stickers, or media
+    if not event.message.text or event.message.sticker or event.message.media or re.search("(?P<url>https?://[^\s]+)", event.message.text):
+        return
+
+    user = await event.client.get_entity(event.message.from_id)
+
+    # Get your own user ID
+    me = await client.get_me()
+
+    # Ignore messages that you wrote
+    if user.id == me.id:
+        return
+
+    if user.last_name:
+        name = f"{user.first_name} {user.last_name}"
+    else:
+        name = f"{user.first_name}"
+
+    print(name + ": " + event.message.text)
+
+    # Check if the message contains Cyrillic symbols
+    if re.search('[а-яА-Я]', event.message.text):
+        # If the message contains Cyrillic symbols, set the voice to a Russian voice identifier string
+        voice_id = 'com.apple.speech.synthesis.voice.milena'
+        message_engine.setVoice_(voice_id)
+    else:
+        # Otherwise, use the default voice
+        message_engine.setVoice_(AppKit.NSSpeechSynthesizer.defaultVoice())
+
+    # Speak the author's name and the message text using the same NSSpeechSynthesizer instance
+    message_engine.startSpeakingString_(name + " says: " + event.message.text)
+    while message_engine.isSpeaking():
+        time.sleep(0.1)
+
+# Get the 20 most recent dialogs
+dialogs = client.loop.run_until_complete(client.get_dialogs(limit=20))
+
+# Print out the list of dialogs for the user to select from
+for i, dialog in enumerate(dialogs):
+    print(f"{i + 1}: {dialog.title}")
+
+# Ask the user to select a chat to read messages from
+selected_dialog = input("Enter the number of the chat to read messages from: ")
+
+# Get the chat entity from the user's selection
+chat = dialogs[int(selected_dialog) - 1].entity
+
+# Start the event loop to listen for new messages in the selected chat
+client.run_until_disconnected()
+
+# Disconnect from the Telegram API
+client.disconnect()
